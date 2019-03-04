dc56d3987d0a09e0dd0166c8a4b69bb97221450e
Hannah Puccini <hannahpuccini@gmail.com>
2019-03-03 21:44:51 -0500

    mid commit

--- Lab3-Sudoku/Sudoku_Solver.c
+++ Lab3-Sudoku/Sudoku_Solver.c
@@ -2,6 +2,53 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int main(void){
+
+
+
+
+#define SIZE 9
+
+//VARS
+
+char board[SIZE][SIZE];
+
+
+//
+
+
+
+void readInput(){
+    FILE *file = fopen("puzzle.txt", "r");
+        char c;
+        while(c != EOF){
+            for(int i=0;i<SIZE;i++){
+                for(int j=0;j<SIZE;i++){
+                    board[i][j] = fgetc(file);
+                    puts(board[i][j]);
+                }
+            }
+        }
+    fclose(file);
+}
+
+
+void * rowSolve(void *threadid)
+{
     
 }
+
+
+int main(void){
+
+
+
+
+
+
+
+int pthread_create(pthread_t *threadINSERT, const pthread_attr_t *attr, void *(start_routine) (void *), void * arg);
+
+
+	
+}
+ 
