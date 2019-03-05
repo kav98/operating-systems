bc72dbf473b16607554e24b7f586c708a31291b1
Hannah Puccini <hannah.puccini@uoit.net>
2019-03-04 22:16:26 -0500

    mid commit

--- Lab3-Sudoku/Sudoku_Solver.c
+++ Lab3-Sudoku/Sudoku_Solver.c
@@ -10,7 +10,7 @@
 
 //VARS
 
-char board[SIZE][SIZE];
+//char board[SIZE][SIZE];
 
 
 
@@ -20,12 +20,57 @@ char board[SIZE][SIZE];
 
 
 int **create_board(){
+    //allocate the space for the rows
     int **board = (int **)malloc(SIZE * sizeof(int));
     
-    
-
+    //allocate space for the coloums
+    for(int i = 0; i<SIZE;i++){
+        board[i] = (int *)malloc(SIZE*sizeof(int));
+    }
     
 }
+
+void free_board(int **board){
+    for(int i = 0; i< SIZE; i++){
+        free(board[i]);
+    }
+}
+
+
+int  **solve_board(){
+
+    while(threads_waiting < num_threads){
+        while(outliers==0){
+            threads_waiting ++;
+            cv.wait();
+            threads_waiting ++;
+        }
+
+        temp = outliers.pop();
+        index, value = MIN_POSSIBLE_VALUES(unsolved board)
+        REDUCE(index, value, board);
+        if valid_grid(board_prime){
+            outliers.push(board_prime)
+            explored.push(board_prime)
+            cv.notify();
+        }
+        else{
+            explored.push(board);
+        }
+    }
+    return board_prime;
+    }
+
+void create_thread(num_threads, board){
+    for(int i=num_threads;i<SIZE){
+        num_threads(SOLVE(outliers, explored, board);
+    }
+}
+
+
+
+
+
 
 
 
@@ -40,6 +85,7 @@ int main(void){
 ///////////GET BOARD////////////////////////////////
 FILE *file = fopen("puzzle.txt", "r");
         char c;
+        char board[SIZE][SIZE];
         while(c != EOF){
             for(int i=0;i<SIZE;i++){
                 for(int j=0;j<SIZE;j++){
@@ -56,7 +102,7 @@ FILE *file = fopen("puzzle.txt", "r");
 
 
 
-int pthread_create(pthread_t *threadINSERT, const pthread_attr_t *attr, void *(start_routine) (void *), void * arg);
+
 
 
 	
